2c48841ecf605766f294b56131ddd0d6fa505cfd
michaelhesch <michael.hesch@gmail.com>
2021-10-28 11:06:31 +0100

    Catch up commit of project from local

---
+++ checkout/__init__.py
@@ -0,0 +1,4 @@
+# Sets default config class for the app.
+# Needs to have this do Django knows about custom ready method
+# so that signals will work. From signals module.
+default_app_config = 'checkout.apps.CheckoutConfig'
